426a0da0b7041384dcc738168d11d64b008ec387
SokolovVasily <ber-sokolov-vasily@outlook.de>
2023-06-06 21:25:10 +0200

    HW_FEjs

---
+++ HW05.06.23FE/script1ha.js
@@ -0,0 +1,52 @@
+// 1.
+/*
+const number = +prompt("Введите число")
+console.log (number * .1)
+*/
+// 2.
+
+/*
+const number = +prompt ("wwedite 1 oe ") 
+
+const number1 =+prompt ("wedite 2 oe")
+
+  if (number > number1) {
+ console.log (number1)   
+} else   {
+ console.log (number)   
+}
+*/
+
+ 
+
+
+
+// 3.
+
+/*
+const number = +prompt("Введите число")
+if(number > 100) {
+    console.log(" Число больше 100")
+} else if (number == 100) {
+    console.log("Число равно 100")
+} else {
+    console.log("Число меньше 100")
+}
+*/
+
+
+// 4.
+
+
+const username = prompt ("wwedite imy")
+const userage  = prompt("wwedite age")
+if (userage > 18) {
+console.log("Hello, " + username )
+} else  {
+console.log("Hi, " + username )
+}
+ 
+
+   
+
+
